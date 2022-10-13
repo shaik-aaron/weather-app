3a3ca9ea5f8e64bf2f64ce0eab867aed8b240fc6
shaik-aaron <shaikaaronbbhs@gmail.com>
2022-10-13 14:55:08 +0530

    API is now called when User hits 'Enter'

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var humid = document.querySelector(".humid");
 var wind = document.querySelector(".wind");
 var img = document.querySelector(".weathericon");
 
+
+//If the User clicks on the search button
+
 button.addEventListener('click',function() {
   fetch(
     "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -36,4 +39,44 @@ button.addEventListener('click',function() {
       wind.innerHTML = "Wind Speed: " + windValue + " m/s";
     })
     .catch((err) => alert("Wrong City name!")); 
+})
+
+//If the User hits enter when in the input field
+
+input.addEventListener('keypress',function(event) {
+  if(event.key == "Enter") {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      input.value +
+      "&appid=7a9d7a1940edef090c0b39d1e737d6ba"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var nameValue = data["name"];
+      var tempValue = data["main"]["temp"];
+      var descValue = data["weather"]["0"]["description"];
+      var humidValue = data["main"]["humidity"];
+      var windValue = data["wind"]["speed"];
+
+      img.setAttribute(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          data["weather"]["0"]["icon"] +
+          ".png"
+      );
+      img.style.display = "block";
+      humid.style.display = "block";
+      wind.style.display = "block";
+
+      tempValue = tempValue - 273.15;
+      tempValue = tempValue.toFixed(1);
+
+      city.innerHTML = "Weather in " + nameValue;
+      temp.innerHTML = tempValue + "°C";
+      desc.innerHTML = descValue;
+      humid.innerHTML = "Humidity: " + humidValue + "%";
+      wind.innerHTML = "Wind Speed: " + windValue + " m/s";
+    })
+    .catch((err) => alert("Wrong City name!"));
+  }
 })
